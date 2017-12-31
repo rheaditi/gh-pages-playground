99047eef93bffb694c75f975af797a8ddc032a00
Aditi Mohanty <rheaditi@users.noreply.github.com>
2017-12-31 18:16:25 +0530

    404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/pages/404.module.css';
+
+import sad404Page from '../images/sad-404-page.svg';
 
 const NotFoundPage = () => (
-  <div>
+  <div className="text-center">
+    <img
+      src={sad404Page}
+      alt="Oops. Not Found!"
+      width="220"
+      height="300"
+      className={styles.image}
+    />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
