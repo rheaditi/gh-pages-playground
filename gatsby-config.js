4022eb9c4c9acc7def74448456458bbec835c0a0
Aditi Mohanty <rheaditi@users.noreply.github.com>
2017-12-31 15:07:55 +0530

    Add Manifest via plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,21 +5,28 @@ const me = {
   location: `Bangalore, India`,
 };
 
+const site = {
+  baseUrl: `http://aditimohanty.com`,
+  title: `${me.name} | Front-End Engineer, Bangalore, India`,
+  description: `${me.name} (@${me.handle}), a female human from ${me.location}\
+- ❤s Javascript, video-games & music. Employed at ClearTax.in.`,
+};
+
 const openGraph = {
   locale: `en_IN`,
   localeAlternate: `en_US`,
 };
 
 const appTheme = {
-  mainColor: '#ff9a9e',
+  mainColor: '#5A50C7',
+  backgroundColor: `#5A50C7`,
 };
 
 module.exports = {
   siteMetadata: {
-    baseUrl: `http://aditimohanty.com`,
-    title: `${me.name} | Front-End Engineer, Bangalore, India`,
-    description: `${me.name} (@${me.handle}), a female human from ${me.location}
-    - ❤s Javascript, video-games & music. Employed at ClearTax.in.`,
+    baseUrl: site.baseUrl,
+    title: site.title,
+    description: site.description,
     keywords: `Aditi Mohanty, Aditi Anomita Mohanty, rheaditi, rhea aditi, Rhea Aditi Mohanty,
     developer, web developer, front end developer, software engineer,
     Bangalore, Bengaluru, India, Karnataka,
@@ -38,6 +45,60 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: me.fullName,
+        short_name: `@${me.handle}`,
+        description: site.description,
+
+        display: 'standalone',
+        background_color: appTheme.backgroundColor,
+        theme_color: appTheme.mainColor,
+
+        start_url: './?utm_source=app_manifest',
+        lang: `en-In`,
+        orientation: 'portrait-primary',
+        icons: [
+          {
+            src: '/img/m-icon/android-icon-36x36.png',
+            sizes: '36x36',
+            type: 'image/png',
+            density: '0.75',
+          },
+          {
+            src: '/img/m-icon/android-icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            density: '1.0',
+          },
+          {
+            src: '/img/m-icon/android-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            density: '1.5',
+          },
+          {
+            src: '/img/m-icon/android-icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            density: '2.0',
+          },
+          {
+            src: '/img/m-icon/android-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            density: '3.0',
+          },
+          {
+            src: '/img/m-icon/android-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            density: '4.0',
+          },
+        ],
       },
     },
   ],
