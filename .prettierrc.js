40802f4bd5021a17df5e9159c0fb9c6c18f7bc53
Aditi Mohanty <rheaditi@users.noreply.github.com>
2017-12-31 12:42:50 +0530

    Bleh

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'singleQuote': true,
+    'tabWidth': 2,
+    'trailingComma': 'es5',
+    'overrides': [
+        {
+            'files': '*.json',
+            'options': {
+                'tabWidth': 2,
+            },
+        },
+    ],
+};
